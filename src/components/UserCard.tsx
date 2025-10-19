848702d3fa849c27384ef0399e2b2d6c42109119
Mohamed Ayman <moaymann007@gmail.com>
2025-10-19 13:25:47 +0300

    feat: improve accessibility in UserCard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { User } from '../services/api';
 import './UserCard.css';
 
 interface UserCardProps {
+  /** User data to display */
   user: User;
 }
 
-const UserCard: React.FC<UserCardProps> = ({ user }) => {
+/**
+ * UserCard component that displays individual user information
+ * Memoized for performance optimization
+ * @param props - Component props
+ * @returns JSX element
+ */
+const UserCard: React.FC<UserCardProps> = memo(({ user }) => {
   return (
-    <div className="user-card">
+    <article className="user-card" role="listitem">
       <div className="user-avatar">
-        <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+        <img
+          src={user.avatar}
+          alt={`${user.first_name} ${user.last_name}`}
+          loading="lazy"
+        />
       </div>
       <div className="user-info">
         <h3 className="user-name">
           {user.first_name} {user.last_name}
         </h3>
       </div>
-    </div>
+    </article>
   );
-};
+});
+
+UserCard.displayName = 'UserCard';
 
 export default UserCard;
